d400a384087cb448a9fc92b1dbefa0948ba69edb
KhalilAMARDJIA <47823346+KhalilAMARDJIA@users.noreply.github.com>
2020-02-06 11:50:19 +0100

    Update open_fda_functions.py

--- open_fda_functions.py
+++ open_fda_functions.py
@@ -20,11 +20,39 @@ def get_meta(url):
     return meta
 
 
-def fda_url(query, count = False, field_count = '', limit = 100, skip = 0):
+import requests
+import json
+import pandas as pd
+import csv
+import os
+import math
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def fda_url(query, data_base="udi", count=False, field_count='', limit=100):
+
     key = '&api_key=WuFW3nIY42Jq1SR9STTbDlQOfYNORGfeHsk5FFU9'
-    api = f'https://api.fda.gov/device/event.json?{key}&search='
+    api = f'https://api.fda.gov/device/{data_base}.json?{key}&search='
+
     if count == True:
         url = f'{api}{query}&count={field_count}'
+        
+
     elif count == False:
-        url = f'{api}{query}&limit={limit}&skip={skip}'
+
+        meta_url = f'{api}{query}'
+        JSON_re = requests.get(meta_url).json()
+        number_results = JSON_re['meta']['results']['total']
+
+        if number_results > 100:
+            n_skips = math.ceil(number_results / 100)
+            for i in range (0, n_skips):
+                skip = 100*i
+                url = f'{api}{query}&limit={limit}&skip={skip}'
+                
+        else: 
+            skip = 0
+            url = f'{api}{query}&limit={limit}&skip={skip}'
+            
     return url
