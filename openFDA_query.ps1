fe32a83ab1b2c40419bf6f909e1d33b84dd9a6f2
KhalilAMARDJIA <47823346+KhalilAMARDJIA@users.noreply.github.com>
2020-03-06 00:48:14 +0100

    Add files via upload

--- openFDA_query.ps1
+++ openFDA_query.ps1
@@ -1,19 +1,25 @@
 Set-Location C:\\Users\\khali\\Desktop\\openFDA
 
 # Variables
-$FDA_part = Read-Host "Please enter which FDA part to query device, drug"
-$database = Read-Host "Please select the database from udi, event, enforcement, 510k, ndc"
-$search = Read-Host "Please enter your query"
+
+# $database = Read-Host "Please select the database from udi, event, enforcement, 510k"
+# $search = Read-Host "Please enter your query"
+$database = "udi"
+$search = "in2bones+AND+PIT'STOP"
 $limit = 100
 
 # request
-$url = "https://api.fda.gov/$FDA_part/$database.json?search=$search&limit=$limit"
+$url = "https://api.fda.gov/device/$database.json?search=$search&limit=$limit"
+$response = Invoke-WebRequest $url
+$response = $response.StatusDescription
 
-$meta = Invoke-WebRequest $url |
-ConvertFrom-Json |
-Select-Object -expand meta
+if ($response -eq "OK") {
+    $meta = Invoke-WebRequest $url |
+    ConvertFrom-Json |
+    Select-Object -expand meta
 
-$n_results = $meta.results.total
+    $n_results = $meta.results.total
+}
 
 if ($n_results -gt 100) {
     $n_skips = [math]::ceiling($n_results / 100) - 1
@@ -24,13 +30,15 @@ else {
 }
 
 $urls = @() # create an empty list
+
 foreach ($i in 0..$n_skips) {
     $skip = 100 * $i
-    $skip_url = "https://api.fda.gov/$FDA_part/$database.json?search=$search&limit=$limit&skip=$skip"
+    $skip_url = "https://api.fda.gov/device/$database.json?search=$search&limit=$limit&skip=$skip"
     $urls += $skip_url
 }
 
 $fda_data = @()
+
 foreach ($url in $urls) {
     $results = Invoke-WebRequest $url |
     ConvertFrom-Json |
