a19060fb55b714f963e4f86177570e29c292e682
KhalilAMARDJIA <47823346+KhalilAMARDJIA@users.noreply.github.com>
2020-12-02 23:34:41 +0100

    Add files via upload

--- event_parser.py
+++ event_parser.py
@@ -13,9 +13,9 @@ with open(filename, encoding='utf8') as file:
 
 
 with open('FDA_event.csv', 'w', newline='', encoding='utf8') as file:
-    writer = csv.writer(file, delimiter=';')
+    writer = csv.writer(file, delimiter='|')
     writer.writerow(['report_numbe', 'manufacturer_d_name','product_problem_flag', 'date_received',
-                     'brand_name', 'device_name', 'product_problems', 'text'])
+                     'brand_name', 'product_problems', 'text'])
 
     for record in event_data:
         report_number = record['report_number']
@@ -23,16 +23,16 @@ with open('FDA_event.csv', 'w', newline='', encoding='utf8') as file:
         product_problem_flag = record['product_problem_flag']
         date_received = record['date_received']
         brand_name = record['device'][0]['brand_name']
-        device_name = record['device'][0]['openfda']['device_name']
 
         try:
-            product_problems = utils.stop_char(record['product_problems'])
+            product_problems = record['product_problems']
         except KeyError:
-            product_problems = []
+            product_problems = ""
 
-        text = utils.stop_char(record['mdr_text']) 
-
-        writer.writerow([report_number, manufacturer_d_name, product_problem_flag, date_received, brand_name, device_name, product_problems, text])
+        text = record['mdr_text'][0]['text_type_code'] + " " + record['mdr_text'][0]['text'].lower() + " " + record['mdr_text'][0]['text_type_code'] + " " + record['mdr_text'][1]['text'].lower()
 
 
-os.startfile('FDA_event.csv')
+
+
+
+        writer.writerow([report_number,manufacturer_d_name, product_problem_flag, date_received, brand_name, product_problems, text])
