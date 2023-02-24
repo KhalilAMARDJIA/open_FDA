606baa2b16cb77d8383cd37ca58e9664a04b4d6a
KhalilAMARDJIA <khalil.amardjia@gmail.com>
2023-02-24 11:24:58 +0100

    GUI DRAFT support

--- GUI_search.py
+++ GUI_search.py
@@ -9,10 +9,8 @@ class Window(QMainWindow):
         self.title = 'Search Database'
         self.left = 700
         self.top = 50
-        self.width = 600
-        self.height = 250
-
-        #self.setWindowIcon(QIcon('icon.png'))
+        self.width = 1000
+        self.height = 300
 
         self.UI()
 
@@ -26,30 +24,49 @@ class Window(QMainWindow):
         self.text1 = QLineEdit(self)
         self.text1.setPlaceholderText('')
         self.text1.move(80, 20)
-        self.text1.resize(500, 22)
+        self.text1.resize(900, 22)
 
         self.label2 = QLabel('Database', self)
         self.label2.move(20, 50)
 
-        dblist = ['event', '510k', 'udi', 'recall', 'enforcement', 
-                  'registrationlisting', 'classification']
+        dblist = ['event', '510k', 'udi', 'recall', 'enforcement', 'registrationlisting', 'classification']
 
         self.combo1 = QComboBox(self)
         self.combo1.addItems(dblist)
         self.combo1.move(80, 50)
         self.combo1.resize(260,22)
 
+        self.label3 = QLabel('From Date (YYYY-MM-DD)', self)
+        self.label3.move(20, 80)
+
+        self.date1 = QDateEdit(self)
+        self.date1.setDisplayFormat('yyyy-MM-dd')
+        self.date1.move(200, 80)
+
+        self.label4 = QLabel('To Date (YYYY-MM-DD)', self)
+        self.label4.move(20, 110)
+
+        self.date2 = QDateEdit(self)
+        self.date2.setDisplayFormat('yyyy-MM-dd')
+        self.date2.move(200, 110)
+
         self.button1 = QPushButton('Search', self)
         self.button1.clicked.connect(self.main)
-        self.button1.move(100, 100)
+        self.button1.move(100, 150)
         
         self.show()
+
 
     def main(self):
         query = self.text1.text()
         search = query.replace(" ", "+AND+")
-
         database = self.combo1.currentText()
+        from_date = self.date1.date().toString('yyyy-MM-dd')
+        to_date = self.date2.date().toString('yyyy-MM-dd')
+
+        if from_date and to_date:
+            date_filter = f'+AND+date_received:[{from_date}+TO+{to_date}]'
+            search += date_filter
 
         from general_openFDA import general_json
         import openFDA_parser
@@ -72,6 +89,10 @@ class Window(QMainWindow):
 
         csv_name = f'saved_csv/{database}_data.csv'
         df.to_csv(csv_name, sep='|', encoding='UTF-8')
+
+        # show message box
+        QMessageBox.information(self, "Search Completed", f"Search for '{query}' in {database} database is completed.")
+
 
 App = QApplication(sys.argv)
 Window = Window()
