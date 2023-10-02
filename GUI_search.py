d4b016950b5518229a66adfcc949e068ebaf8ffd
KhalilAMARDJIA <khalil.amardjia@gmail.com>
2023-10-02 17:31:37 +0200

    GUI update

--- GUI_search.py
+++ GUI_search.py
@@ -6,6 +6,7 @@ from general_openFDA import general_json
 import openFDA_parser
 import sys
 
+
 # Define the search_data function without caching
 def search_data(query, database, from_date, to_date):
     data, database, last_updated, n_results = general_json(query=query, database=database)
@@ -24,6 +25,7 @@ def search_data(query, database, from_date, to_date):
 
 # Set Streamlit app title and layout
 st.set_page_config(layout="wide")
+col1, col2, col3 = st.columns([2,2,1])
 st.title('Search openFDA Database')
 st.sidebar.title('Settings')
 
@@ -62,22 +64,21 @@ if search_button:
 
     try:
         df.to_csv(csv_path, sep='|', encoding='UTF-8')
-        st.success("CSV file saved successfully.")
     except Exception as e:
         st.error(f"Error saving CSV file: {str(e)}")
 
-    st.success(f"""
+    col1.success(f"""
                Search for openFDA in {saved_database} database is completed.
-               
-               Last Updated: {last_updated}
-
-               Number of Results: {n_results}
-
                """)
-    st.download_button(label="Download CSV", data=df.to_csv(index=False), key='download_csv')
+    
+    col3.metric(label= "Number of Results", value=n_results)
+    col3.metric(label= "Last Updated", value=last_updated)
+
+    col2.download_button(label="Download CSV", data=df.to_csv(index=False), key='download_csv')
     
     # Store the DataFrame in session state
     st.session_state.df = df
+
 
 # Function to execute event_plot.py
 def execute_event_plot():
@@ -85,8 +86,8 @@ def execute_event_plot():
     script_path = "Misc/event_plot.py"
     subprocess.run([virtualenv_python, script_path])
 
-# Create a button to execute the script
-if st.button("Execute event_plot.py"):
+# Create a button to execute the script if the selected database is "event"
+if database == 'event' and st.button("Execute event_plot.py"):
     execute_event_plot()
     st.success("event_plot.py executed successfully!")
 
